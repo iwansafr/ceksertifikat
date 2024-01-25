c08455ed85a803f2152f4e099f40e267fa0ba27f
iwansafr <iwansafr@gmail.com>
2024-01-25 10:56:55 +0700

    default check sertifikat

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('main');
-});
+Route::get('/', CertificateCheck::class);
 
 Route::get('add-certificate',function(){
     return view('add-certificate');
